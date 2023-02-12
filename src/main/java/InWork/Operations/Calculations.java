870702c74375cc96f6bb974d97c90320c83d0e42
PC <tarzanar1421@gmail.com>
2023-02-12 13:11:57 +0100

    DataPPList do poprawy bo cos nie kmini + import PP zrobiony w excel api + wstepne rozpisanie danych potrzebnych do operacji.

---
+++ src/main/java/InWork/Operations/Calculations.java
@@ -0,0 +1,18 @@
+package InWork.Operations;
+
+public class Calculations {
+
+    // operacje na danych z import PP
+    // do danych dociagnac z bazy danych odpowieniej w zaleznosci od fabryki
+    // finalne dane ktore beda potrzebne do operacji (sku,liczba palet,max liczby palet,czas produkcji, data startu, godzina startu,rynek docelowy)
+    //sku = z import PP
+    //liczba palet = QNT(import PP) / net(z database odpowiedniego)
+    //max liczby palet = 22500/ gross(z database odpowiedniego)
+    //czas produkcji = (SDate+Stime) - (Edate+Etime) / liczba palet
+    //Data startu = z import pp
+    //Godzina startu z import pp
+    //rynek docelowy = dest(database)
+
+
+
+}
