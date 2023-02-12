2e8dcf29d85adbc0226a9f270a1b6757af4a1b38
PC <tarzanar1421@gmail.com>
2023-02-12 12:32:43 +0100

    zmienne

---
+++ src/main/java/InWork/DataBase/DataStructure/DataPP.java
@@ -0,0 +1,77 @@
+package InWork.DataBase.DataStructure;
+
+public class DataPP {
+
+    int SKU;
+    double STime;
+    double SDate;
+    double ETime;
+    double EDate;
+    double QNT;
+    String Lane;
+
+    public DataPP()
+    {
+
+    }
+
+
+    public int getSKU() {
+        return SKU;
+    }
+
+    public void setSKU(int SKU) {
+        this.SKU = SKU;
+    }
+
+    public double getSTime() {
+        return STime;
+    }
+
+    public void setSTime(double STime) {
+        this.STime = STime;
+    }
+
+    public double getSDate() {
+        return SDate;
+    }
+
+    public void setSDate(double SDate) {
+        this.SDate = SDate;
+    }
+
+    public double getETime() {
+        return ETime;
+    }
+
+    public void setETime(double ETime) {
+        this.ETime = ETime;
+    }
+
+    public double getEDate() {
+        return EDate;
+    }
+
+    public void setEDate(double EDate) {
+        this.EDate = EDate;
+    }
+
+    public double getQNT() {
+        return QNT;
+    }
+
+    public void setQNT(double QNT) {
+        this.QNT = QNT;
+    }
+
+    public String getLane() {
+        return Lane;
+    }
+
+    public void setLane(String lane) {
+        Lane = lane;
+    }
+
+
+
+}
